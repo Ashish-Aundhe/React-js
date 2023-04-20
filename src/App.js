ff69e836aa283ed8ffcb9b3f765ae3e26b3d47b1
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-04-21 03:52:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // import logo from './logo.svg';
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForms from './components/TextForms';
+// import TextForms from './components/TextForms';
 // let name = "Ashu01"
 function App() {
   return (
     <>
       <Navbar title="textUtils" aboutText="About TextUtils" />
       <div className="container my-3">
-        <TextForms heading="Enter the text to anylyst below" />
+        {/* <TextForms heading="Enter the text to anylyst below" /> */}
+        <About></About>
       </div>
     </>
   );
