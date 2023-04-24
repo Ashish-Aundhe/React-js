2d35a90e97c23949e4867db94fc7e3100476a7dc
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-04-25 04:15:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 // import logo from './logo.svg';
+import './App.css';  
+import React, { useState } from 'react';
 
-import { useState } from 'react';
-import './App.css';
 // import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForms from './components/TextForms';
+import Navbar from './components/Navbar'; 
+import TextForms from './components/TextForms'; 
+
 // let name = "Ashu01"
 function App() {
-  const 
+  const [mode, setMode] = useState('light'); //Whether dark mode is enablw or not 
+
+    const toggleMode = ()=>{
+      if(mode === 'light'){
+        setMode = 'dark'
+      }
+      else{
+        setMode = 'light'
+      }
+    }
   return (
     <>
-      <Navbar title="textUtils" aboutText="About TextUtils" />
+     {/* aboutText="About TextUtils" */}
+      <Navbar title="textUtils" mode= {mode} toggleMode = {toggleMode} />   
       <div className="container my-3">
         <TextForms heading="Enter the text to anylyst below" />
         {/* <About></About> */}
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -15,7 +15,21 @@ export default function TextForms(props) {
         // console.log("On change");
         setText(event.target.value)
     }
-
+    const handleClearClick = () =>{
+        let newText = '';
+        setText(newText)
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+    const handleCopy = () => {
+        console.log ("i am Copy");
+        var text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0, 9999);
+        navigator.clipboard.writeText(text.value);
+    }
     const [text, setText] = useState("Enter text here2");
     //text = "new text"; // wrong  way to change the state
     // setText("new text"); //corrrect way to change the state
@@ -28,6 +42,9 @@ export default function TextForms(props) {
                 </div>
                 <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Clear Extra Spaces</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
 
             </div>
             <div className="container my-3">
