166458706252ed005a26f0c6e4144b741e4a8f63
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-05-03 23:21:59 +0530

    Add files via upload

--- src/Alert.js
+++ src/Alert.js
@@ -4,7 +4,7 @@ function Alert(props) {
   return (
     props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
     {props.alert.type}: {props.alert.msg}
-    
+     
   </div>
   );
 }
