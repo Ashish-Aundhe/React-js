853ec8a4d82542503b36c25954666b1d6bcd23c6
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-05-11 23:50:58 +0530

    error is present is due to the router version is olf

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import Navbar from "./components/Navbar";
 import TextForms from "./components/TextForms";
 import Alert from "./Alert";
 
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState("light"); //Whether dark mode is enablw or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) => {
+  const showAlert = (message, type) =>{
     setAlert({
       msg: message,
       type: type,
@@ -33,22 +40,30 @@ function App() {
     }
   };
   return (
-    <>
+    
+    <Router>
       {/* aboutText="About TextUtils" */}
       
         <Navbar title="textUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
-        <div className="container my-3">
-          
+        <div className="container my-3"> 
 
-          <About />
-          <TextForms
+        <Routes>
+          <Route path="/About" element={ <About />}> 
+          </Route>
+
+          <Route path="/"  element={<TextForms
                 showAlert={showAlert}
                 heading="Enter the text to anylyst below"
                 mode={mode}
-              />
+               />}>
+          </Route>
+        </Routes>
+
+          {/* <About /> */}
+          
         </div>
-    </>
+      </Router>
   );
 }
 export default App;
