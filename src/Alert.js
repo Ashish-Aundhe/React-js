bd660305bd753fc9c3e3cde6b4c6a22122869f1b
Ashish-Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-05-19 23:25:13 +0530

    Add files via upload

--- src/Alert.js
+++ src/Alert.js
@@ -6,8 +6,8 @@ function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   }
   return (
-    props.alert && <div className = {'alert alert-${props.alert.type}   
-    alert-dismissible fade show'} role="alert">
+    props.alert && <div className = {`alert alert-${props.alert.type}   
+    alert-dismissible fade show`} role="alert">
     <strong>{capitalize(props.alert.type)} </strong>: {props.alert.msg} 
   </div>  
   ) 
