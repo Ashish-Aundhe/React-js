442a6cd420b5d49d7127f6495b5f75b7865e7847
Ashish Aundhe <123938575+Ashish-Aundhe@users.noreply.github.com>
2023-06-08 03:57:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,77 +1,17 @@
 // import logo from './logo.svg';
-import "./App.css";
-import React, { useState } from "react";
+import './App.css';
+ 
+import React, { Component } from 'react'
+import NavBar from './component/NavBar';
+import News from './component/News';
 
-import About from "./components/About";
-import Navbar from "./components/Navbar";
-import TextForms from "./components/TextForms";
-import Alert from "./Alert";
-
-import { HashRouter as Router, Routes, Route,} from "react-router-dom";
-
-function App() {
-  const [mode, setMode] = useState("light"); //Whether dark mode is enable or not
-  const [alert, setAlert] = useState(null);
-
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 1500);
-  };
-
-  const removeBodyClasses=()=>{
-    document.body.classList.remove('bg-light')
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-sucess')
-  }
-
-  const toggleMode = (cls) => {
-    removeBodyClasses();
-    console.log(cls)
-    document.body.classList.add('bg-'+cls)
-    if (mode === "light") {
-      setMode("dark"); //we cant use setMode='dark' bcz its state varibale
-      document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode has been enabled", "Sccess");
-    } else {
-      setMode("white");
-      document.body.style.backgroundColor = "white";
-      showAlert("Light mode has been enabled", "success");
-    }
-  };
-  return (
-    <>
-    <Router>
-      {/*<Navrbar title="TextUtils" aboutText="About TextUtils" /> */}
-
-      <Navbar title="textUtils" mode={mode} toggleMode={toggleMode} aboutText="About us" />
-      <Alert alert={alert} />
-      <div className="container my-3">
-
-                 {/* <Switch> is used in old version, it replsced by Routes */}
-        <Routes>
-          <Route exact path="/about" element={<About mode={mode} />}> </Route>
-
-          <Route
-           exact path="/"
-            element={
-              <TextForms
-                showAlert={showAlert}
-                heading="Enter the text to anylyst below"
-                mode={mode}
-              />
-            }
-          ></Route>
-        </Routes>
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+         <NavBar/>
+         <News/>
       </div>
-    </Router>
-    </>
-  );
+    )
+  }
 }
-export default App;
